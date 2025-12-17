f56ac9f0966509dd91e8137889953e0fba20a05a
Maksym Prokopov <mprokopov@gmail.com>
2025-12-17 03:49:32 +0100

    Integrate Prometheus for API call tracking
    Added Prometheus metrics to track API call count.

--- main.go
+++ main.go
@@ -5,7 +5,15 @@ import (
     "fmt"
     "log"
     "net/http"
+    "github.com/prometheus/client_golang/prometheus"
+    "github.com/prometheus/client_golang/prometheus/promauto"
+    "github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+var counter = promauto.NewCounter(prometheus.CounterOpts{
+    Name: "api_calls_total",
+    Help: "The total number of processed API calls",
+})
 
 type Simple struct {
     Name        string
@@ -21,6 +29,10 @@ func handler(w http.ResponseWriter, r *http.Request) {
     simple := SimpleFactory(r.Host)
 
     jsonOutput, _ := json.Marshal(simple)
+    // otherwise favicon.ico also triggers it 🤷‍♂️
+    if r.URL.Path == "/" {
+        counter.Inc()
+    }
 
     w.Header().Set("Content-Type", "application/json")
 
@@ -29,6 +41,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
     fmt.Println("Server started on port 4444")
+    http.Handle("/metrics", promhttp.Handler())
     http.HandleFunc("/", handler)
     log.Fatal(http.ListenAndServe(":4444", nil))
 }
