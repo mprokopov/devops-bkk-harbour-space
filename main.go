27eb4e3fd9a26183458b28f3cf913f46c82d4e28
Maksym Prokopov <mprokopov@gmail.com>
2025-12-18 04:04:28 +0100

    fix the test

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ type Simple struct {
 }
 
 func SimpleFactory(host string) Simple {
-    return Simple{"Hello", "Students", "break test"}
+    return Simple{"Hello", "Students", host}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
