c3650ff6e8c0a51618d5e08610b2cead16d12185
Maksym Prokopov <mprokopov@gmail.com>
2025-12-18 05:09:18 +0100

    Change greeting from 'Hello' to 'Goodbye'

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ type Simple struct {
 }
 
 func SimpleFactory(host string) Simple {
-    return Simple{"Hello", "Students", host}
+    return Simple{"Goodbye", "Students", host}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
